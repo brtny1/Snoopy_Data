6a823dce2edb6b76ebd324ec4d5d3cd2c7ab3f20
s80249 <>
2021-10-27 00:11:57 +0000

    using a list comprehension to create 2D array

--- main.py
+++ main.py
@@ -7,9 +7,12 @@ import csv
 with open("FilterOff_OpenWindow.TXT" , 'r') as txt:
     txt2csv = csv.reader(txt, delimiter = "\t")
     #Creating 2D array with data from txt file:
-    """
+    """ 
     data = []
     for row in txt2csv:
         data.append(row)
+    print(data)
     """
+    #rewriting lines 11 to 13 using list comprehension:
+    data = [row for row in txt2csv]
     
