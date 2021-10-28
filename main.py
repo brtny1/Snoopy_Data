a9137dc471dec188b717a0dffe3fb375b7cf03e7
s80249 <>
2021-10-28 00:33:33 +0000

    creating csv file and adding data to it.

--- main.py
+++ main.py
@@ -4,15 +4,22 @@
 import csv
 
 #Read tab-delimited file:
-with open("FilterOff_OpenWindow.TXT" , 'r') as txt:
-    txt2csv = csv.reader(txt, delimiter = "\t")
-    #Creating 2D array with data from txt file:
-    """ 
-    data = []
-    for row in txt2csv:
-        data.append(row)
-    print(data)
-    """
-    #rewriting lines 11 to 13 using list comprehension:
-    data = [row for row in txt2csv]
+with open("FilterOff_OpenWindow.TXT" , 'r') as txtFile:
+  txt2list = csv.reader(txtFile, delimiter = "\t")
+  #Creating 2D array with data from txt file:
+  """ 
+  data = []
+  for row in txt2list:
+      data.append(row)
+  print(data)
+  """
+  #rewriting lines 11 to 13 using list comprehension:
+  data = [row for row in txt2list]
     
+# write to comma-delimited file:
+with open("FilterOff_OpenWindow.csv", 'w') as csvFile:
+  # creating csv file:
+  list2csv = csv.writer(csvFile, quotechar='', quoting=csv.QUOTE_NONE)
+  # adding each sublist from the data array to a seperate row in csvFile:
+  list2csv.writerows(data)
+
